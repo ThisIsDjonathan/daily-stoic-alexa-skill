3b7418082252c342e45c7be8e1aaaae65e2b642d
Djonathan Krause <email@djonathan.com>
2024-03-22 10:19:57 -0300

    fix hard coded file path

--- make-it-louder.py
+++ make-it-louder.py
@@ -1,16 +1,18 @@
 from pydub import AudioSegment
 import os
 
-def louder(filename):
-    file_path = f'assets/audios/portuguese/{filename}'
+INCREASE_VOLUME_IN_DB = 13
+
+def louder(language_to_increase_volume, filename):
+    file_path = f'assets/audios/{language_to_increase_volume}/{filename}'
     song = AudioSegment.from_mp3(file_path)
-    louder_song = song + 15
+    louder_song = song + INCREASE_VOLUME_IN_DB
     louder_song.export(file_path, format='mp3')
 
 def main(language_to_increase_volume):
     for filename in os.listdir(f'assets/audios/{language_to_increase_volume}'):
         try:
-            louder(filename)
+            louder(language_to_increase_volume, filename)
             print(f'{filename} done.')
         except Exception as e:
             print(f'Error on file {filename}: {e}')
