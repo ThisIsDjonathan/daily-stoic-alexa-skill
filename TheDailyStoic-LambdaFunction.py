33c4699293629974b190e733c8ed88a95fbd80ae
Djonathan Krause <email@djonathan.com>
2024-06-25 11:32:14 -0300

    updating amazon lambda

--- TheDailyStoic-LambdaFunction.py
+++ TheDailyStoic-LambdaFunction.py
@@ -4,13 +4,14 @@ import uuid
 
 def lambda_handler(event, context):
     day_of_year = datetime.now().timetuple().tm_yday
-    bucket_name = os.environ['BUCKET_NAME']
+    s3_bucket_name = os.environ['S3_BUCKET_NAME']
     object_key = str(day_of_year) + '.mp3'
+    audio_url = f"https://{s3_bucket_name}.s3.amazonaws.com/audios/portuguese/{object_key}"
 
     return {
         "uid": str(uuid.uuid4()),
         "updateDate": datetime.now().isoformat(),
         "titleText": "Diário Estoico",
         "mainText": "",
-        "streamUrl": f"https://{bucket_name}.s3.amazonaws.com/{object_key}"
+        "streamUrl": audio_url
       }
